31d1d2fb8fb8806aba02544e9be0cbaccec581c6
pootle <iainmalcolm1@googlemail.com>
2018-05-13 09:09:06 +0100

    support for multiple static folders
    matches the multiple folder support changes to webserv.py

--- config.py
+++ config.py
@@ -2,16 +2,21 @@
 
 serverdefaults={
     'port':8000,            # this is the port the webserver will respond to
-    'servefrom': 'static',  # this is the relative or absolute path to the folder containing static files 
+    'servefrom': {          # this is the list of folders we can use to find files.
+            'static'  : 'static',                                # the default folder for entries with no 'foldname' key
+            'appstat' : '/home/pi/gitbits/pipywebserv/static',   # further entries where 'foldname' is one of these keys
+        },    
     'getpaths' : {          # paths to be handled by pywebhandler.do_GET for GETs
-        ''            : {'pagetype': 'static',      'pagefile': 'homepage.html'},
-        'index'       : {'pagetype': 'static',      'pagefile': 'homepage.html'},
-        'base1.css'   : {'pagetype': 'static',      'pagefile': 'base1.css'},
-        'smoothie.js' : {'pagetype': 'static',      'pagefile': 'smoothie.js'},
-        'dostream'    : {'pagetype': 'eventstream', 
-                'streams':{'pistatus':{'streamtype': 'generator', 'modfunc': 'pistatus', 'period': 1.9},
+        ''               : {'pagetype': 'static',      'pagefile': 'homepage.html'},
+        'index'          : {'pagetype': 'static',      'pagefile': 'homepage.html'},
+        'base1.css'      : {'pagetype': 'static',      'pagefile': 'base1.css'},
+        'smoothie.js'    : {'pagetype': 'static',      'pagefile': 'smoothie.js'},
+        'pipyscripts.js' : {'pagetype': 'static',      'pagefile': 'pipyscripts.js'},
+        'speshull'       : {'pagetype': 'static',      'pagefile': 'specialtext.html', 'folder': 'appstat'},
+        'dostream'       : {'pagetype': 'eventstream', 
+                'streams'   :{'pistatus':{'streamtype': 'generator', 'modfunc': 'pistatus', 'period': 1.9},
                 }
-                        },
-        'favicon.ico' : {'pagetype': 'static',      'pagefile': 'rasppi.ico', 'log':1},
-    }
+                           },
+        'favicon.ico'    : {'pagetype': 'static',      'pagefile': 'rasppi.ico', 'log':1},
+        }
 }
